d8072e153ff9a0221bfd82ffa97e95c6b806b8be
Tony Owens <anthony.owens1987@gmail.com>
2023-02-23 21:21:51 -0800

    Updating to v2.1.4

--- Formula/terracreds.rb
+++ Formula/terracreds.rb
@@ -1,8 +1,8 @@
 class Terracreds < Formula
   desc "Credential helper for Terraform Automation and Collaboration Software"
   homepage "https://github.com/tonedefdev/terracreds"
-  url "https://github.com/tonedefdev/terracreds/archive/refs/tags/v2.1.3.tar.gz"
-  sha256 "84857ff1b3e55107c0553be9d1d716bb2a88211c5dd9f8e0fad8060ca018a431"
+  url "https://github.com/tonedefdev/terracreds/archive/refs/tags/v2.1.4.tar.gz"
+  sha256 "3e9840f507bafa516c27826da33c14f157e7ce68b7c17e3c8b1431523a36d709"
   license "Apache-2.0"
 
   depends_on "go" => :build
