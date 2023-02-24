d44dec1503bb1fcf0c3db32be504d329c4855366
Tony Owens <anthony.owens1987@gmail.com>
2023-02-24 13:42:17 -0800

    Added correct package

--- Formula/terracreds.rb
+++ Formula/terracreds.rb
@@ -2,7 +2,7 @@ class Terracreds < Formula
   desc "Credential helper for Terraform Automation and Collaboration Software"
   homepage "https://github.com/tonedefdev/terracreds"
   url "https://github.com/tonedefdev/terracreds/archive/refs/tags/v2.1.4.tar.gz"
-  sha256 "3e9840f507bafa516c27826da33c14f157e7ce68b7c17e3c8b1431523a36d709"
+  sha256 "5541049f60872751ed87674e9871d5bcfd620d399f435853a0b1041a60b92efb"
   license "Apache-2.0"
 
   depends_on "go" => :build
